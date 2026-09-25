82dce04b8a9f755f01a79d126b6400f95f9d3acb
UmemaZuha <umemazuha@gmail.com>
2026-09-26 00:08:33 +0530

    Fix password modification

--- models/user.js
+++ models/user.js
@@ -41,7 +41,7 @@ const userSchema = new Schema(
 userSchema.pre("save", function (next) {
     const user = this;
 
-    if (!user.isModified("password")) return;
+    if (!user.isModified("password")) return next();
 
     const salt = randomBytes(16).toString("hex");
 
